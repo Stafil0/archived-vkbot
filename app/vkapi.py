bbcbf9c76ce8f2a92c8ae976184b41b9e2727d00
Stafilo <stafilo13@gmail.com>
2018-11-08 00:54:06 +0400

    поправил api

--- app/vkapi.py
+++ app/vkapi.py
@@ -6,6 +6,17 @@ session = vk.Session()
 api = vk.API(session, v=version)
 
 
-def send_message(user_id, token, message, attachment=""):
-    api.messages.send(access_token=token, user_id=str(user_id), message=message, attachment=attachment)
+def send_peer(peer_id, token, message, attachment=''):
+    api.messages.send(access_token=token, peer_id=str(peer_id),
+                      message=message, attachment=attachment)
+
+
+def send_chat(chat_id, token, message, attachment=''):
+    api.messages.send(access_token=token, chat_id=str(chat_id),
+                      message=message, attachment=attachment)
+
+
+def send_user(user_id, token, message, attachment=''):
+    api.messages.send(access_token=token, user_id=str(user_id),
+                      message=message, attachment=attachment)
 
--- responses/hello.py
+++ responses/hello.py
@@ -2,11 +2,11 @@ from repository import responses
 
 
 def hello():
-    message = 'Дороу.'
+    message = 'Дороу'
     return message, ''
 
 
 response = responses.Response()
-response.keys = ['привет', 'hello', 'дратути', 'здравствуй', 'здравствуйте']
+response.keys = ['привет', 'hello', 'дратути', 'здравствуй', 'здравствуйте', 'дороу']
 response.description = 'Приветствие'
 response.process = hello
